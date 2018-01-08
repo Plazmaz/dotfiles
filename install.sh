428078c951fd136e731bb67d3ebff26e08cd19a5
Dylan Katz <git@dylankatz.com>
2018-01-08 09:43:54 -0700

    Added sh+ alias for django shell_plus and bashrc changes

--- install.sh
+++ install.sh
@@ -17,31 +17,17 @@ fish -c "omf install bobthefish; omf theme bobthefish; exit"
 echo -e "\e[93m>>>\e[0m Switching default shell..."
 chsh -s `which fish`
 
-sudo apt-get install -y git terminator xfce4
+sudo apt-get install -y git terminator
 
 function windir() {
     echo "/mnt/$1" | sed 's/\\/\//g' | sed 's/\b\(.\):/\L\1/g';
 }
 
-echo -e "\e[93m>>>\e[0m Installing fonts (This will take a while)..."
+echo -e "\e[93m>>>\e[0m Installing Fonts (This may take a while)..."
 if [[ ! -d fonts ]]; then
     git clone https://github.com/powerline/fonts.git --depth=1
-    powershell.exe -Command \&{"$(cat fonts/install.ps1)"}
     ./fonts/install.sh
 fi
-#powershell.exe -executionpolicy bypass -File fonts\\install.ps1
-
-#echo -e "\e[93m>>>\e[0m Installing themes..."
-#fish -c "omf install bobthefish; omf theme bobthefish; exit"
-
-echo -e "\e[93m>>>\e[0m Linking shortcuts..."
-MY_DOCUMENTS=$(windir `powershell.exe -c "Write-Host ([Environment]::GetFolderPath('MyDocuments')) -NoNewLine"`)
-DESKTOP=$(windir `powershell.exe -c "Write-Host ([Environment]::GetFolderPath('Desktop')) -NoNewLine"`)
-ln -sv "$MY_DOCUMENTS" ~
-ln -sv "$DESKTOP" ~
-#ln -sv "/mnt/c/Users/$USER/Pictures" ~
-#ln -sv "/mnt/c/Users/$USER/Videos" ~
-#ln -sv "/mnt/c/Users/$USER/Music" ~
 
 echo -e "\e[93m>>>\e[0m Checking for old backup files..."
 if [[ ! -f ~/.bash_aliases_old && ! -f ~/.profile_old  && ! -f ~/.bashrc_old && ! -f ~/.gitconfig_old ]]; then
